8fbce4bac77b90d7a881015db919bb9d77a1d0c9
Себастьян Парерро <kremezny@ukr.net>
2021-09-25 20:27:03 +0300

    second commit

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ let totalСost = clockPrice + phonePrice + kettlePrice;
 console.log(`Вартість всіх товарів: ${totalСost}`);
 
 //Відкиньте копійки у всіх товарів, потім – складіть цілу частину вартості кожного товару між собою. Округлення використовувати в МЕНЬШУ сторону.
-clockPriceRoundDown = Math.floor(15.678);
-phonePriceRoundDown = Math.floor(123.965);
-kettlePriceRoundDown = Math.floor(90.2345);
+clockPriceRoundDown = Math.floor(clockPrice);
+phonePriceRoundDown = Math.floor(phonePrice);
+kettlePriceRoundDown = Math.floor(kettlePrice);
 let totalСostRoundDown = clockPriceRoundDown + phonePriceRoundDown + kettlePriceRoundDown;
 
 //Виведіть суму товарів округлену до сотень. (Наприклад якщо вийшло 260, то виведіть 300)
@@ -34,20 +34,19 @@ console.log(`Cереднє значення цін: ${(totalСost / 3).toFixed(2
 /* (Більш складне) Створіть змінну, в якої збережіть випадкову знижку (використовуйте функцію Math.random).
 Зробіть клієнту випадкову знижку та виведіть суму до оплати округлену до 2 знаків після коми.
 Виведіть чистий прибуток, якщо клієнт заплатив зі знижкою та собівартість товарів рівно в два рази нижче їх ціни ?
-(Приклад: ціна була 260.66, знижка склала 10%, клієнт заплатив на 26 меньше, собівартість = 260 / 2 -> 130. Чистий прибуток = 130 - 26 -> 104) (Приклад 2: ціна була 100,
-знижка склала 90 %, клієнт заплатив на 90 меньше, собівартість = 100 / 2 -> 50. Чистий прибуток = 50 - 90 -> -40) */
+(Приклад: ціна була 260.66, знижка склала 10%, клієнт заплатив на 26 меньше, собівартість = 260 / 2 -> 130. Чистий прибуток = 130 - 26 -> 104)
+(Приклад 2: ціна була 100, знижка склала 90 %, клієнт заплатив на 90 меньше, собівартість = 100 / 2 -> 50. Чистий прибуток = 50 - 90 -> -40) */
 
 let price = 260.66;
-let discount = 90;
-//let discount = Math.round(Math.random() * 100);
+let discount = Math.floor(Math.random() * (99 - 1 + 1)) + 1;
 console.log(`Випадкова знижка: ${discount}%`);
 
-let amountToPay = Math.ceil(price * (100 - discount) / 100).toFixed(2);
-console.log(`Сума до оплати: ${amountToPay}`);
+let amountToPay = Math.ceil((price * (100 - discount) / 100) * 100) / 100;
+console.log(`Сума до оплати: ${amountToPay.toFixed(2)}`);
 
 let difference = price - amountToPay;
 let costPrice = price / 2;
-let netProfit = Math.floor(costPrice - difference).toFixed(2);
-console.log(netProfit);
+let netProfit = Math.floor((costPrice - difference) * 100) / 100;
+console.log(`Чистий прибуток ${netProfit.toFixed(2)}`);
 
 
