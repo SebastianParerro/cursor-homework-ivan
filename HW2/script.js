0a3fdb7ce825feff83e5b8de374e8649370ee482
Себастьян Парерро <kremezny@ukr.net>
2021-09-26 20:58:03 +0300

    first commit

---
+++ HW2/script.js
@@ -0,0 +1,21 @@
+let N;
+do {
+	N = +prompt('Введіть число N', ' ');
+} while (!Number.isInteger(N));
+
+let M;
+do {
+	M = +prompt('Введіть число M', ' ');
+} while (!Number.isInteger(M));
+
+let skipEvenNumbers = confirm("Потрібно пропускати парні числа?");
+
+let sum = 0;
+for (let i = N; i <= M; i++) {
+	if (skipEvenNumbers && i % 2 === 0) {
+		continue;
+	} else {
+		sum += i;
+	}
+}
+console.log(sum);
