26c797cd7b03a1ce66b1585b9d1ac3f8155c1430
Себастьян Парерро <kremezny@ukr.net>
2021-11-01 09:45:44 +0200

    HW13

--- HW12/script.js
+++ HW12/script.js
@@ -1,25 +1,22 @@
 'use strict';
 
-function playSound(e) {
-	const audio = document.querySelector(`audio[data-key ="${e.keyCode}"]`);
-	const key = document.querySelector(`.key[data-key ="${e.keyCode}"]`);
-	if (!audio) return;
-	audio.currentTime = 0;
-	audio.play();
-	key.classList.add('playing');
+function playSoundBykey(key) {
+  const audio = document.querySelector(`audio[data-key="${key}"]`);
+  const keyElement = document.querySelector(`.key[data-key="${key}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  keyElement.classList.add('playing');
 }
-
-function removeTransition(e) {
-	if (e.propertyName !== 'transform') return;
-	this.classList.remove('playing');
-}
-
-function removeClass() {
-	const keys = document.querySelectorAll('.key');
-	keys.forEach(key => key.classList.remove('playing'));
-}
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
-window.addEventListener('keyup', removeClass);
+const keys = document.querySelectorAll('.keys>div[data-key]');
+keys.forEach(key => {
+  key.addEventListener('click', event => {
+    playSoundBykey(event.currentTarget.dataset.key);
+  })
+  key.addEventListener('transitionend', event => {
+    event.currentTarget.classList.remove('playing');
+  })
+})
+window.addEventListener('keydown', (event) => {
+  playSoundBykey(event.keyCode);
+});
---
+++ HW13/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Створіть функцію, яка повертає проміс getRandomChinese(length). Функція працює таким чином:
+
+// Запускається цикл(підказка: можна використовувати while) length раз, кожен прохід циклу дивимось на останні 5 цифр результату виклику методу Date.now(). 
+// Наприклад отримали const sign = 16086;.
+// Конвертуємо отриману цифу в рядок String.fromCharCode(sign); та чекаємо 50 ms
+// Функція gerRandomChinese(length) повинна повернути рядок довжиною (length) з китайськими ієрогліфами. Час роботи проміса має складати length * 50ms.
+// (Якщо викликати getRandomChinese(4), отримаємо результат "촛궻簽紙" за 200ms
+
+function getRandomChinese(length) {
+	return new Promise(function (resolve, reject) {
+		let i = 0;
+		const str = [];
+
+		while (i < length) {
+			setTimeout(() => {
+				const sign = Date.now() % 100000;
+			  const symbol = String.fromCharCode(sign);
+				str.push(symbol)
+
+				if (str.length === length) {
+					resolve(str);
+				}
+			}, i * 50);
+
+			i++;
+		}
+	});
+};
+
+getRandomChinese(4).then((result) => {
+	console.log(result);
+});
