8c796c5f617a8b0af469da7f0df65b47a292ec90
Себастьян Парерро <kremezny@ukr.net>
2021-11-06 13:04:44 +0200

    HW14

--- HW14/script.js
+++ HW14/script.js
@@ -15,20 +15,20 @@ let wookiee = false;
 let peopleClicked = false;
 
 const photo = {
-	"https://swapi.dev/api/people/1/": "https://static.wikia.nocookie.net/rustarwars/images/d/d9/Luke-rotjpromo.jpg/revision/latest/scale-to-width-down/400?cb=20061127144734",
+	"https://swapi.dev/api/people/1/": "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Luke_Skywalker.png/220px-Luke_Skywalker.png",
 	"https://swapi.dev/api/people/2/": "https://img.discogs.com/asQcQW243t9t4jC4n4UUlaU7DoM=/600x600/smart/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/A-376068-1574719343-4313.jpeg.jpg",
 	"https://swapi.dev/api/people/3/": "https://upload.wikimedia.org/wikipedia/ru/3/39/R2-D2_Droid.png",
 	"https://swapi.dev/api/people/4/": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Star_Wars_-_Darth_Vader.jpg/274px-Star_Wars_-_Darth_Vader.jpg",
-	"https://swapi.dev/api/people/5/": "https://static.wikia.nocookie.net/rustarwars/images/8/83/Sw4-Leia2.png/revision/latest?cb=20170913054540",
+	"https://swapi.dev/api/people/5/": "https://upload.wikimedia.org/wikipedia/ru/0/0a/Leia_Organa_Ep5_DVD.jpg",
 	"https://swapi.dev/api/people/10/": "https://upload.wikimedia.org/wikipedia/en/thumb/3/32/Ben_Kenobi.png/220px-Ben_Kenobi.png",
-	"https://swapi.dev/api/people/13/": "https://static.wikia.nocookie.net/starwars/images/4/48/Chewbacca_TLJ.png/revision/latest?cb=20210106001220",
+	"https://swapi.dev/api/people/13/": "https://upload.wikimedia.org/wikipedia/commons/f/fd/Chewbacca_%26_Jawa.jpg",
 	"https://swapi.dev/api/people/14/": "http://geekcity.ru/wp-content/uploads/2016/07/han-solo-episode-v.jpg",
 	"https://swapi.dev/api/people/18/": "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Wedge_Antilles-Denis_Lawson-Star_Wars_%281977%29.jpg/220px-Wedge_Antilles-Denis_Lawson-Star_Wars_%281977%29.jpg",
-	"https://swapi.dev/api/people/20/": "https://static.wikia.nocookie.net/rustarwars/images/d/d6/Yoda_SWSB.png/revision/latest?cb=20171222112613",
+	"https://swapi.dev/api/people/20/": "https://upload.wikimedia.org/wikipedia/ru/9/96/CGIYoda.jpg",
 	"https://swapi.dev/api/people/21/": "https://upload.wikimedia.org/wikipedia/en/8/8f/Emperor_RotJ.png",
 	"https://swapi.dev/api/people/22/": "https://upload.wikimedia.org/wikipedia/en/3/3e/FettbobaJB.png",
-	"https://swapi.dev/api/people/23/": "https://static.wikia.nocookie.net/rustarwars/images/5/59/IG-88f.jpg/revision/latest?cb=20190226130253",
-	"https://swapi.dev/api/people/24/": "https://static.wikia.nocookie.net/rustarwars/images/d/d2/Bossk.jpg/revision/latest?cb=20100212063334",
+	"https://swapi.dev/api/people/23/": "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/IG-11_Star_Wars.jpg/220px-IG-11_Star_Wars.jpg",
+	"https://swapi.dev/api/people/24/": "https://upload.wikimedia.org/wikipedia/commons/9/9d/Bossk_-_The_Bounty_Picker_Upper_%28cropped%29.jpg",
 	"https://swapi.dev/api/people/25/": "https://m.media-amazon.com/images/M/MV5BMjI3MjA0NTQwOV5BMl5BanBnXkFtZTcwMzcxMTY2Mw@@._V1_.jpg",
 	"https://swapi.dev/api/people/26/": "https://mikescollection.files.wordpress.com/2012/01/lobot2.jpg",
 };
